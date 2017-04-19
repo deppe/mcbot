07ff68341c53b8d2d350d3ec78177db10ff1cc29
Adam Deppe <adam.deppe@gmail.com>
2017-04-18 23:33:49 -0400

    Switching to chatbot

--- mcbot.py
+++ mcbot.py
@@ -4,13 +4,16 @@ import time
 import random
 from textblob import TextBlob
 from slackclient import SlackClient
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 class MCBot(object):
-    def __init__(self, token):
+    def __init__(self, token, chatbot):
         self.token = token
         self.delay = 1
         self.user_id = None
         self.client = SlackClient(token)
+        self.chatbot = chatbot
 
     def connect(self):
         self.client.rtm_connect()
@@ -33,8 +36,10 @@ class MCBot(object):
 
     def handle_msg(self, text, channel):
         if channel[0] == 'D' or self.is_mention(text):
-            parser = Parser(text)
-            self.client.api_call("chat.postMessage", channel=channel, text=parser.respond(), as_user=True)
+            response = self.chatbot.get_response(text)
+            self.client.api_call("chat.postMessage", channel=channel, text=str(response), as_user=True)
+            os.remove('database.db')
+            Trainer('data.txt').train(self.chatbot)
 
     def is_mention(self, text):
         return re.search('<@' + self.user_id + '>', text)
@@ -46,19 +51,33 @@ class MCBot(object):
                 return member['id']
 
 
-class Parser(object):
-    GREETINGS = [':smiley: My day was okay. How was yours?', 'When are you doing it?', 'When are you gonna do it? Stop ignoring the question']
+class Trainer(object):
+    def __init__(self, filename):
+        self.filename = filename
 
-    def __init__(self, text):
-        self.parsed = TextBlob(text)
-        
-    def respond(self):
-        return random.choice(self.GREETINGS)
+    def train(self, chatbot):
+        with open(self.filename) as f:
+            M = ''
+            F = ''
+            for line in f:
+                person, text = map(lambda x: x.strip(), line.split(':'))
 
+                if person == 'M':
+                    if F:
+                        chatbot.train([M.strip(), F.strip()])
+                        print ([M, F])
+                        M = ''
+                        F = ''
+                    M += text + ' '
+                elif person == 'F':
+                    F += text + ' '
 
 def main():
+    chatbot = ChatBot('mcbot')
+    chatbot.set_trainer(ListTrainer)
+    Trainer('data.txt').train(chatbot)
     token = os.environ.get('SLACK_BOT_TOKEN')
-    bot = MCBot(token)
+    bot = MCBot(token, chatbot)
     bot.connect()
     bot.start()
     
